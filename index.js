144d3f05db385e75eb39f922ebbd494103ba9dfd
XtianFreolo <f60@umbc.edu>
2025-05-09 20:45:55 -0400

    task 1

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // look back at the <readme.md> file for some hints //
 // working API key //
 const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+
+
+async function giphyRequest() {
+    const response = await axios.get(`http://api.giphy.com/v1/gifs/trending?api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    console.log(response);
+  }
+  giphyRequest();
