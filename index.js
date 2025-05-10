f7fba46e5897bfad3f41b6e034a8eee6acf82ef2
XtianFreolo <f60@umbc.edu>
2025-05-09 20:50:51 -0400

    answering questions in read.me as a comment in ".js"

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // look back at the <readme.md> file for some hints //
 // working API key //
+
+
+// read me questions 
+//Task 1
+// The response.data.data shows a large array of objects-
+// -preasumely the GIFs at Giphy.  
+// Then if you use "data-limit-10" into the code, it only shows-
+// -you 10 objects, meaning 10 data in the array. 
+
+
 const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 
 
